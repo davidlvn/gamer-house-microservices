9f2deaa02459017fab3b168f4716924cd4410a7a
davidlvn <lamydavid38@gmail.com>
2022-01-30 13:55:45 +0100

    Authentication microservice isolated

--- server/createServer.js
+++ server/createServer.js
@@ -7,15 +7,7 @@ exports.createServer = void 0;
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var authenticationRoutes_1 = __importDefault(require("../routes/authenticationRoutes"));
-var constants_1 = require("../constants");
 var healthRoutes_1 = __importDefault(require("../routes/healthRoutes"));
-var gamerRoutes_1 = __importDefault(require("../routes/gamerRoutes"));
-var authenticationMiddleware_1 = __importDefault(require("../middleware/authenticationMiddleware"));
-var postRoutes_1 = __importDefault(require("../routes/postRoutes"));
-var postTagRoutes_1 = __importDefault(require("../routes/postTagRoutes"));
-var postCommentRoutes_1 = __importDefault(require("../routes/postCommentRoutes"));
-var groupRoutes_1 = __importDefault(require("../routes/groupRoutes"));
-var gameRoutes_1 = __importDefault(require("../routes/gameRoutes"));
 /** Create express app and open backend endpoints */
 function createServer() {
     var app = express_1.default();
@@ -25,12 +17,6 @@ function createServer() {
     // Backend endpoints
     app.use('/authentication', authenticationRoutes_1.default);
     app.use('/health', healthRoutes_1.default);
-    app.use('/comment', postCommentRoutes_1.default);
-    app.use('/games', gameRoutes_1.default);
-    app.use('/gamers', gamerRoutes_1.default);
-    app.use('/posts', postRoutes_1.default);
-    app.use('/postTags', postTagRoutes_1.default);
-    app.use('/groups', groupRoutes_1.default);
     return app;
 }
 exports.createServer = createServer;
